2e350e42ecedbd36bb56c3b6bc2774ba7a0408ee
Todor Kostov <kostovtd@gmail.com>
2016-12-17 23:22:51 +0200

    ButterKnife included.

--- app/src/main/java/com/kostovtd/bemyguest/HomeActivity.java
+++ app/src/main/java/com/kostovtd/bemyguest/HomeActivity.java
@@ -2,6 +2,11 @@ package com.kostovtd.bemyguest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.UserManager;
+import android.widget.LinearLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Todor on 12/17/2016.
@@ -11,11 +16,15 @@ public class HomeActivity extends Activity {
 
     private static final String TAG = HomeActivity.class.getSimpleName();
 
+    @BindView(R.id.root_container)
+    LinearLayout rootContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ButterKnife.bind(this);
     }
 
 
