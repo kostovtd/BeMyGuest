27ce784aa19c8e604b007be27df141abe46a57c9
Todor Kostov <kostovtd@gmail.com>
2016-12-17 22:56:49 +0200

    Basic implementation for Home screen included.

---
+++ app/src/main/java/com/kostovtd/bemyguest/HomeActivity.java
@@ -0,0 +1,38 @@
+package com.kostovtd.bemyguest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Todor on 12/17/2016.
+ */
+
+public class HomeActivity extends Activity {
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
