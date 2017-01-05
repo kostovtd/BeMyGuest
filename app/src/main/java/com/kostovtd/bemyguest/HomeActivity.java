9488b127fae983ab0bb675930ab3c284bf420792
Todor Kostov <kostovtd@gmail.com>
2017-01-06 00:01:24 +0200

    MinSdkVersion changed to 18. libsuperuser included. Basic tests for executing SU commands included.

--- app/src/main/java/com/kostovtd/bemyguest/HomeActivity.java
+++ app/src/main/java/com/kostovtd/bemyguest/HomeActivity.java
@@ -1,8 +1,10 @@
 package com.kostovtd.bemyguest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.UserManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -10,8 +12,11 @@ import android.widget.Toast;
 
 import com.kostovtd.bemyguest.util.PermissionsChecker;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import eu.chainfire.libsuperuser.Shell;
 
 /**
  * Created by Todor on 12/17/2016.
@@ -20,6 +25,7 @@ import butterknife.ButterKnife;
 public class HomeActivity extends Activity {
 
     private static final String TAG = HomeActivity.class.getSimpleName();
+    private static final int REQUEST_CODE = 1001;
 
     private static final String[] PERMISSIONS = new String[] {
             //TODO add permissions here
@@ -32,27 +38,31 @@ public class HomeActivity extends Activity {
     @BindView(R.id.root_container)
     LinearLayout rootContainer;
 
-    @BindView(R.id.button_count_users)
-    Button bCountUsers;
+    @BindView(R.id.button_test)
+    Button bTest;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: hit");
         setContentView(R.layout.activity_home);
 
         ButterKnife.bind(this);
 
         permissionsChecker = new PermissionsChecker(this);
 
-        bCountUsers.setOnClickListener(new View.OnClickListener() {
+        bTest.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                UserManager userManager = (UserManager) getSystemService(USER_SERVICE);
-                int numberOfUsers = userManager.getUserCount();
-                String toastMessage = "Number of users: " + numberOfUsers;
-                Toast.makeText(HomeActivity.this, toastMessage, Toast.LENGTH_LONG).show();
-            }
+            public void onClick(View v) {
+                Log.d(TAG, "SU available: " + Shell.SU.available());
+                Log.d(TAG, "SU version: " + Shell.SU.version(true));
+                List<String> resultString = Shell.SU.run("ls /data/system/users/");
+                Log.d(TAG, "result size: " + resultString.size());
+                for(String result : resultString) {
+                    Log.d(TAG, result + "\n");
+                }
+             }
         });
     }
 
@@ -80,5 +90,15 @@ public class HomeActivity extends Activity {
 
     private void startPermissionsActivity() {
         PermissionsActivity.startActivityForResult(this, PermissionsActivity.PERMISSION_REQUEST_CODE, PERMISSIONS);
+    }
+
+    private void testCreateUser() {
+        Intent createUserIntent = UserManager.createUserCreationIntent("userName", "accoutName", null, null);
+        startActivityForResult(createUserIntent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
